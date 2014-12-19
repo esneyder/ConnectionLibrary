d052b75f389c288474cd8538b0db3d3aec3d41c9
esneyder <edesalla17@gmail.com>
2014-12-19 15:18:06 -0500

    librera genrica csharp

---
+++ databaseConnection/DbConnection.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace databaseConnection
+{
+    public class DbConnection
+    {
+        #region Attributes
+        private string fileParameters;
+        private string connectionString;
+        private string error;
+        private bool connectionExists;
+        private SqlConnection cnn;        
+        private SqlCommand cmd;
+        private SqlDataReader dr;         
+        #endregion
+
+        #region Properties
+        public string Error
+        {
+            get { return error; }
+             
+        }
+
+        public SqlConnection Cnn
+        {
+            get { return cnn; }
+             
+        }
+
+        public SqlDataReader Dr
+        {
+            get { return dr; }
+           
+            
+        }
+        #endregion
+
+        #region Constructor
+        public DbConnection(string fileParameters)
+        {
+            this.fileParameters = fileParameters;
+            connectionExists = false;
+            cnn = new SqlConnection();
+            cmd = new SqlCommand();
+        } 
+        #endregion
+
+        #region Private Methods
+        private bool GenerateConnectionString()
+        {
+            try
+            {
+                Parameters parameter = new Parameters();
+                if (!parameter.GenerateConnectionString(fileParameters))
+                {
+                    error = parameter.Error;
+                    parameter = null;
+                    return false;
+                }
+                else
+                {
+                    connectionString = parameter.ConnectionString;
+                    parameter = null;
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                error = ex.Message;
+                return false;
+            }
+        }
+        #endregion
+
+        #region Public Methods
+        public bool OpenConnection()
+        {
+            if (!GenerateConnectionString()) return false; 
+           
+                cnn.ConnectionString = connectionString;
+                try
+                {
+                    cnn.Open();
+                    connectionExists = true;
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    error = "Did not open the connection, " + ex.Message;
+                    connectionExists = false;
+                    return false;
+                }
+           
+        }
+
+        public void CloseConnection()
+        {
+            try
+            {
+                cnn.Close();
+                connectionExists = false;
+            }
+            catch (Exception ex)
+            {
+                error = "Did not closed the connection, " + ex.Message;
+            }
+        }
+
+        public DataTable GetData(string procedureName, string[] parameterName, 
+            params Object[] parameterValues)
+        {
+            DataTable dt = new DataTable();
+            cmd.Connection = cnn;
+            cmd.CommandText = procedureName;
+            cmd.CommandType = CommandType.StoredProcedure;
+            if (procedureName.Length != 0 && parameterName.Length == parameterValues.Length)
+            {
+                int i = 0;
+                foreach (String parameter in parameterName)
+                    cmd.Parameters.AddWithValue(parameter,parameterValues[i++]);
+                try
+                {
+                    dr = cmd.ExecuteReader();
+                    dt.Load(dr);
+                    CloseConnection();
+                    return dt;
+                }
+                catch (Exception ex)
+                {
+                    error = ex.Message;
+                }
+            }
+            return dt;
+        }
+
+        public int ExecuteProcedures(string procedureName, string[] parameterName, 
+            params Object[] parameterValues)
+        {
+            cmd.Connection = cnn;
+            cmd.CommandText = procedureName;
+            cmd.CommandType = CommandType.StoredProcedure;
+            if (procedureName.Length != 0 && parameterName.Length == parameterValues.Length)
+            {
+                int i = 0;
+                foreach (String parameter in parameterName)
+                    cmd.Parameters.AddWithValue(parameter, parameterValues[i++]);
+                try
+                {
+                    return cmd.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    error = ex.Message;
+                }
+            }
+            return 0;
+        }
+        #endregion
+    }
+}
---
+++ databaseConnection/Parameters.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace databaseConnection
+{
+    public class Parameters
+    {
+        #region Attributes
+        private string server;
+        private string database;
+        private string user;
+        private string password;
+        private bool integrateSecurity;
+        private string fileParameters;
+        private string connectionString;
+        private string error;        
+        private XmlDocument xml=new XmlDocument();
+        private XmlNode node; 
+        #endregion
+
+        #region Properties
+        public string ConnectionString
+        {
+            get { return connectionString; }
+           
+        }
+
+        public string Error
+        {
+            get { return error; }
+            
+        } 
+        #endregion
+
+        #region Constructor
+        public Parameters()
+        {
+            server = "";
+            database = "";
+            user = "";
+            password = "";
+            integrateSecurity = true;
+            fileParameters = "";
+            connectionString = "";
+            error = "";
+            
+        } 
+        #endregion
+
+        #region Public Methods
+        public bool GenerateConnectionString(string fileNameParameters)
+        {
+            fileParameters = Application.StartupPath + "\\" + fileNameParameters;
+            try
+            {
+                // Read xml file
+                xml.Load(fileParameters);
+
+                // Read the first attribute of the connection string
+                node = xml.SelectSingleNode("//Server");
+
+                // Return the value of the node
+                server = node.InnerText;
+
+                node = xml.SelectSingleNode("//Database");
+                database = node.InnerText;
+
+                node = xml.SelectSingleNode("//User");
+                user = node.InnerText;
+
+                node = xml.SelectSingleNode("//Password");
+                password = node.InnerText;
+
+                node = xml.SelectSingleNode("//IntegrateSecurity");
+                integrateSecurity = Convert.ToBoolean(node.InnerText);
+
+                if (integrateSecurity) // Windows authentication
+                {
+                    connectionString = "Data Source=" + server + ";Initial Catalog=" +
+                        database + ";Integrate Security=true";
+                }
+                else // Sql authentication
+                {
+                    connectionString = "Data Source=" + server + ";Initial Catalog=" +
+                        database +
+                        ";User Id=" + user +
+                        ";Password=" + password;
+                      
+                }
+                xml = null;
+                return true;
+            }
+            catch (Exception ex)
+            {
+                error = ex.Message;
+                return false;
+            }
+        } 
+        #endregion
+    }
+}
